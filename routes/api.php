b0509245e78835bd5bf468b977dfa5c00ef2bfd5
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-02-24 16:49:15 +0100

    AUTH: api endpoints and routes edited

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// PUBLIC ROUTES
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// PROTECTED ROUTES
+Route::middleware('auth:sanctum')->group(function () {
+    // Example route to get the authenticated user's information
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
+
+    // Get the authenticated user's information
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+});
